12d518ae3c789246add8cf13021ccdc62410424a
Sandip <srai68141@gmail.com>
2024-05-15 22:01:03 +0545

    feat: added dashboard layout

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,29 @@ import { createBrowserRouter } from 'react-router-dom'
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
+import DashBoardLayout from './layouts/DashBoardLayout';
+import ProductPage from './pages/ProductPage';
 
 
  const router = createBrowserRouter(
    [
     {
-        path: '/',
-        element: <HomePage />,
+        path: 'dashboard',
+        element: <DashBoardLayout/>,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+
+            {
+                path:'product',
+                element: <ProductPage/>,
+
+            },
+            
+        ],
+        
     },
 
         {
