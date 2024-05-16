de60567ba0b20a456645ec98f56b82c137255c74
Sandip <srai68141@gmail.com>
2024-05-16 22:57:23 +0545

    feat: added login functionality using usestate and useNaviagte hooks

--- src/layouts/DashBoardLayout.tsx
+++ src/layouts/DashBoardLayout.tsx
@@ -27,7 +27,7 @@ const DashBoardLayout = () => {
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
-                to ="#"
+                to = {'home'}
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Home className="h-4 w-4" />
@@ -44,11 +44,11 @@ const DashBoardLayout = () => {
                 </Badge>
               </Link>
               <Link
-                to ="#"
+                to ={'product'}
                 className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
               >
                 <Package className="h-4 w-4" />
-                Products{" "}
+                Products
               </Link>
               <Link
                 to ="#"
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,9 +2,27 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Check if the email and password are correct
+    if (email === 'ramdevaba@patanjali.com' && password === 'asd') {
+      // Redirect to the dashboard
+      navigate('/dashboard');
+    } else {
+      // Show an error message
+      alert('Invalid email or password');
+    }
+  }
+
+
     return(
         <section className="flex justify-center items-center h-screen">
      
@@ -15,16 +33,18 @@ const LoginPage = () => {
           Enter your email below to login to your account.
         </CardDescription>
       </CardHeader>
+      <Form onSubmit={handleSubmit}>
       <CardContent className="grid gap-4">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Input id="email" type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="m@example.com" required />
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
+          <Input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} required />
         </div>
       </CardContent>
+      </Form>
       <CardFooter>
         <div className="w-full">
         <Button className="w-full">Sign in</Button>
