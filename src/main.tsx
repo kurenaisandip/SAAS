31562dbb8a86d6991dba22675cff070423ce5294
Sandip <srai68141@gmail.com>
2024-05-22 22:28:54 +0545

    feat: added tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,24 @@ import App from './App.tsx'
 import './index.css'
 import router from './router.tsx'
 import  { RouterProvider } from 'react-router-dom'
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
-    <RouterProvider router = {router}>
+    <QueryClientProvider client={queryClient}>
+    <RouterProvider router = {router} />
+      </QueryClientProvider>
 
-    </RouterProvider>
+    {/* </RouterProvider> */}
   </React.StrictMode>,
 )
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -60,3 +60,4 @@ const handleLogin = () => {
 }
 
 export default LoginPage;
+
